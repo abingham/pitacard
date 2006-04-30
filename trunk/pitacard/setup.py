ddfa1e4fab70ad8912ea86ca57ea6d55f92faaf0
abingham <abingham@ce24fe9b-e611-0410-8ffd-b180f63eacb8>
2006-04-30 13:19:42 +0000

    updated setup.py to include more relevant metadata.
    updated MANIFEST to include INSTALL and AUTHORS
    in the source distribution.
    
    
    git-svn-id: https://pitacard.svn.sourceforge.net/svnroot/pitacard@10 ce24fe9b-e611-0410-8ffd-b180f63eacb8

--- trunk/pitacard/setup.py
+++ trunk/pitacard/setup.py
@@ -4,12 +4,23 @@ from distutils.core import setup
 import os
 
 setup(name='pitacard',
-      version='0.7',
+      version='0.8',
       description='Leitner-method flash card system',
       author='Austin Bingham',
       author_email='austin.bingham@gmail.com',
       url='http://austin.bingham.googlepages.com/pitacard',
+      license='GPL',
       packages=['pitacard'],
       scripts=['scripts/pitacard'],
-      package_data={'pitacard' : [os.path.join('glade','pitacard.glade')]},
+      package_data={'pitacard' : [os.path.join('glade','pitacard.glade'),
+				  'INSTALL',
+ 				  'AUTHORS']},
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: X11 Applications :: GTK',
+                   'Intended Audience :: End Users/Desktop',
+                   'License :: OSI Approved :: GNU General Public License (GPL)',
+                   'Operating System :: POSIX',
+                   'Programming Language :: Python',
+                   'Topic :: Education'
+                   ]
      )
